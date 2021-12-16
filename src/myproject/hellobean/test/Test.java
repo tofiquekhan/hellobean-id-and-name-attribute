828ac0778ade0a2fe5fd90b115f3710cc029120c
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2021-12-16 13:48:22 +0530

    Using single id attribute

---
+++ src/myproject/hellobean/test/Test.java
@@ -0,0 +1,15 @@
+package myproject.hellobean.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import myproject.hellobean.beans.HelloBean;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("/myproject/hellobean/resources/applicationContext.xml");
+		HelloBean helloBean = (HelloBean)context.getBean("helloBean");
+		System.out.println(helloBean.sayHello());
+	}
+}
